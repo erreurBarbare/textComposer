763189839a9805638c46670e6eaf510e1b1ab878
Barbara Fauth <bg.fauth@gmail.com>
2022-11-18 23:07:20 +0100

    integrate series configuration into template generation (wip)

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import datetime
+import json
 
 from jinja2 import Environment, PackageLoader, select_autoescape
+from jsonpath_ng import parse, ext
+
+# TODO: Create config file
+FILENAME_OUTPUT = "test_output.txt"
+PATH_SERIES = "examples/series.json"
 
 
 def change_days(date, num_days):
     return date + datetime.timedelta(days=num_days)
 
 
-def datetimeformat(value, format='%d.%m.%Y'):
-    return value.strftime(format)
+def datetimeformat(value, date_format='%d.%m.%Y'):
+    return value.strftime(date_format)
+
+
+def get_relevant_series(json_series):
+    jsonpath_get_ids = parse('$.series[*].id')
+    ids = [match.value for match in jsonpath_get_ids.find(json_series)]
+
+    print("The following series are available:")
+    for i in ids:
+        print("-", i)
+    print()
+    # TODO: User selects desired series -> via number or name (bash dialog)
+    # TODO: Add error handling
+    relevant_id = input("-> Please enter desired series name: ")
+
+    jp_expr = ext.parse(f"$.series[?(@.id=={relevant_id})].variables")
+    return jp_expr.find(json_series)
 
 
 def main():
@@ -16,21 +38,26 @@ def main():
                       autoescape=select_autoescape())
     env.filters['change_days'] = change_days
     env.filters['datetimeformat'] = datetimeformat
-    template = env.get_template("am_before_workshop.txt")
+    template = env.get_template("test-template.txt.j2")
 
-    filename = "test_output.txt"
+    file_series = open(PATH_SERIES)
+    series = json.load(file_series)
+
+    relevant_series = get_relevant_series(series)
+    params = relevant_series[0].value
 
     workshop_date = datetime.datetime(2022, 12, 3)
 
     content = template.render(
         firstname="Valentina",
-        workshop="aare_meter",
+        os="win",
         date=workshop_date,
-        time="from 9.30am to 4.30pm",
-        os="win"
+        workshop=params.get("workshop"),
+        time=params.get("time"),
+        price=params.get("price") or "Free entry, collection"
     )
 
-    with open(filename, mode="w", encoding="utf-8") as message:
+    with open(FILENAME_OUTPUT, mode="w", encoding="utf-8") as message:
         message.write(content)
 
 
