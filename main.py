0f60bb0c2a7d30d10d80779f801a2c42544506f1
Barbara Fauth <bg.fauth@gmail.com>
2022-11-19 21:47:05 +0100

    Refactoring

--- main.py
+++ main.py
@@ -20,11 +20,9 @@ def datetimeformat(value, date_format=DATE_FORMAT_MACHINE_READABLE):
     return value.strftime(date_format)
 
 
-def get_relevant_series():
-    file_series = open(PATH_SERIES)
-    series = json.load(file_series)
+def get_relevant_series_id(series_json):
     jsonpath_get_ids = parse('$.series[*].id')
-    ids = [match.value for match in jsonpath_get_ids.find(series)]
+    ids = [match.value for match in jsonpath_get_ids.find(series_json)]
 
     print("The following series are available:")
     for i in ids:
@@ -32,10 +30,7 @@ def get_relevant_series():
     print()
     # TODO: User can select desired series via number or name (bash dialog)
     # TODO: Add error handling
-    relevant_id = input("-> Please enter desired series name: ")
-
-    jp_expr = ext.parse(f"$.series[?(@.id=={relevant_id})].variables")
-    return jp_expr.find(series)
+    return input("-> Please enter desired series name: ")
 
 
 def setup(env):
@@ -65,18 +60,28 @@ def get_template_vars(env, series_params):
     return template_vars_dict
 
 
+def get_series_attribute(series_id, series, attribute):
+    jp_expr = ext.parse(f"$.series[?(@.id=={series_id})].{attribute}")
+    attr = jp_expr.find(series)
+    return attr[0].value
+
+
 def main():
     env = Environment(loader=PackageLoader("main"),
                       autoescape=select_autoescape())
     setup(env)
 
+    file_series = open(PATH_SERIES)
+    series = json.load(file_series)
+
+    series_id = get_relevant_series_id(series)
+    series_vars = get_series_attribute(series_id, series, "variables")
+    #TODO: Support multiple blocks files
+    series_blocks_file = get_series_attribute(series_id, series, "source_file")
+
     template = env.get_template(TEMPLATE)
-
-    relevant_series = get_relevant_series()
-    series_vars = relevant_series[0].value
-
     template_vars = get_template_vars(env, series_vars)
-    # TODO Parse int and dates correctly
+    # TODO Parse int and dates (and enums?) correctly
     workshop_date = datetime.datetime(2022, 12, 3)
     template_vars.update({"date": workshop_date})
 
