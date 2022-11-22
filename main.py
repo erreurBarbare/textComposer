a105b4dd90761e4b2dfd0c6a94cd331e27256d2c
Barbara Fauth <bg.fauth@gmail.com>
2022-11-22 23:02:26 +0100

    Generate template from blocks

--- main.py
+++ main.py
@@ -10,17 +10,25 @@ import utils
 import input_utils
 
 configs = properties.get_properties()
+SERIES = "series"
+BLOCKS = "blocks"
 
 
-def get_series_attribute(series_id, series, attribute):
-    jp_expr = ext.parse(f"$.series[?(@.id=={series_id})].{attribute}")
-    attr = jp_expr.find(series)
+def get_attribute_of_single_object(object_name, object_id, json_object, attribute):
+    jp_expr = ext.parse(f"$..{object_name}[?(@.id=={object_id})].{attribute}")
+    attr = jp_expr.find(json_object)
     return attr[0].value
 
 
-def get_blocks_values(blocks, attribute):
-    jsonpath_get_ints = parse(f"$.blocks[*].{attribute}")
-    return [match.value for match in jsonpath_get_ints.find(blocks)]
+def get_attribute_of_all_objects(object_name, json_object, attribute):
+    jsonpath_get_ints = parse(f"$..{object_name}[*].{attribute}")
+    return [match.value for match in jsonpath_get_ints.find(json_object)]
+
+
+def generate_template(relevant_blocks, blocks_json, template_path):
+    with open(template_path, mode="w", encoding="utf-8") as new_template:
+        for b in relevant_blocks:
+            new_template.write(get_attribute_of_single_object(BLOCKS, b, blocks_json, "value") + "\n")
 
 
 def main():
@@ -29,21 +37,24 @@ def main():
     jinja_utils.setup(env)
 
     file_series = open(configs.get("PATH_SERIES").data)
-    series = json.load(file_series)
+    series_json = json.load(file_series)
 
-    series_id = input_utils.get_relevant_series_id(series)
-    series_vars = get_series_attribute(series_id, series, "variables")
-    series_blocks_path = get_series_attribute(series_id, series, "source_file")
+    series_id = input_utils.get_relevant_series_id(series_json)
+    series_vars = get_attribute_of_single_object(SERIES, series_id, series_json, "variables")
+    blocks_path = get_attribute_of_single_object(SERIES, series_id, series_json, "source_file")
+    series_relevant_blocks = get_attribute_of_single_object(SERIES, series_id, series_json, "blocks")
 
-    file_blocks = open(series_blocks_path)
+    file_blocks = open(blocks_path)
     blocks_json = json.load(file_blocks)
 
-    int_variables = utils.flatten(get_blocks_values(blocks_json, "integer_vars"))
-    date_variables = utils.flatten(get_blocks_values(blocks_json, "date_vars"))
-    enum_variables = utils.flatten(get_blocks_values(blocks_json, "enum_vars"))
-
+    generate_template(series_relevant_blocks, blocks_json,
+                      configs.get("TEMPLATE_FOLDER").data + configs.get("TEMPLATE").data)
     template = env.get_template(configs.get("TEMPLATE").data)
-    template_vars = input_utils.get_template_vars(env, series_vars, int_variables, date_variables, enum_variables)
+
+    int_vars = utils.flatten(get_attribute_of_all_objects(BLOCKS, blocks_json, "integer_vars"))
+    date_vars = utils.flatten(get_attribute_of_all_objects(BLOCKS, blocks_json, "date_vars"))
+    enum_vars = utils.flatten(get_attribute_of_all_objects(BLOCKS, blocks_json, "enum_vars"))
+    template_vars = input_utils.get_template_vars(env, series_vars, int_vars, date_vars, enum_vars)
 
     content = template.render(template_vars)
 
