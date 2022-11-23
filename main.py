0e35c85e78ba85862f90ee8207362238b6963d02
Barbara Fauth <bg.fauth@gmail.com>
2022-11-23 19:42:38 +0100

    refactoring, use UTF-8

--- main.py
+++ main.py
@@ -1,64 +1,49 @@
-import json
-
 from jinja2 import Environment, PackageLoader, select_autoescape
-from jsonpath_ng import parse, ext
 
+import composer_utils as cu
+import input_utils
 import jinja_utils
 import properties
-
-import utils
-import input_utils
 
 configs = properties.get_properties()
 SERIES = "series"
 BLOCKS = "blocks"
 
 
-def get_attribute_of_single_object(object_name, object_id, json_object, attribute):
-    jp_expr = ext.parse(f"$..{object_name}[?(@.id=={object_id})].{attribute}")
-    attr = jp_expr.find(json_object)
-    return attr[0].value
-
-
-def get_attribute_of_all_objects(object_name, json_object, attribute):
-    jsonpath_get_ints = parse(f"$..{object_name}[*].{attribute}")
-    return [match.value for match in jsonpath_get_ints.find(json_object)]
-
-
-def generate_template(relevant_blocks, blocks_json, template_path):
-    with open(template_path, mode="w", encoding="utf-8") as new_template:
-        for b in relevant_blocks:
-            new_template.write(get_attribute_of_single_object(BLOCKS, b, blocks_json, "value") + "\n\n")
-
-
 def main():
+    # setup
     env = Environment(loader=PackageLoader("main"),
                       autoescape=select_autoescape())
     jinja_utils.setup(env)
 
-    file_series = open(configs.get("PATH_SERIES").data)
-    series_json = json.load(file_series)
+    # load series
+    series_json = cu.load_json_from_file(configs.get("PATH_SERIES").data)
 
+    # select relevant series
     series_id = input_utils.get_relevant_series_id(series_json)
-    series_vars = get_attribute_of_single_object(SERIES, series_id, series_json, "variables")
-    blocks_path = get_attribute_of_single_object(SERIES, series_id, series_json, "source_file")
-    series_relevant_blocks = get_attribute_of_single_object(SERIES, series_id, series_json, "blocks")
+    series_vars = cu.get_attribute_of_single_object(SERIES, series_id, series_json, "variables")
 
-    file_blocks = open(blocks_path)
-    blocks_json = json.load(file_blocks)
+    # generate template from blocks
+    blocks_path = cu.get_attribute_of_single_object(SERIES, series_id, series_json, "source_file")
+    series_relevant_blocks = cu.get_attribute_of_single_object(SERIES, series_id, series_json, "blocks")
 
-    generate_template(series_relevant_blocks, blocks_json,
-                      configs.get("TEMPLATE_FOLDER").data + configs.get("TEMPLATE").data)
+    blocks_json = cu.load_json_from_file(blocks_path)
+
+    cu.generate_template(series_relevant_blocks, blocks_json,
+                         configs.get("TEMPLATE_FOLDER").data + configs.get("TEMPLATE").data)
     template = env.get_template(configs.get("TEMPLATE").data)
 
-    int_vars = utils.flatten(get_attribute_of_all_objects(BLOCKS, blocks_json, "integer_vars"))
-    date_vars = utils.flatten(get_attribute_of_all_objects(BLOCKS, blocks_json, "date_vars"))
-    enum_vars = utils.flatten(get_attribute_of_all_objects(BLOCKS, blocks_json, "enum_vars"))
-    template_vars = input_utils.get_template_vars(env, series_vars, int_vars, date_vars, enum_vars)
+    # define the variables values for the final text
+    int_vars = cu.flatten(cu.get_attribute_of_all_objects(BLOCKS, blocks_json, "integer_vars"))
+    date_vars = cu.flatten(cu.get_attribute_of_all_objects(BLOCKS, blocks_json, "date_vars"))
+    enum_vars = cu.flatten(cu.get_attribute_of_all_objects(BLOCKS, blocks_json, "enum_vars"))
+    text_variables_values = input_utils.get_template_vars(env, series_vars, int_vars, date_vars, enum_vars)
 
-    content = template.render(template_vars)
+    # generate final text
+    content = template.render(text_variables_values)
 
-    with open(configs.get("FOLDER_OUTPUT").data + configs.get("FILENAME_OUTPUT").data, mode="w", encoding="utf-8") as message:
+    with open(configs.get("FOLDER_OUTPUT").data + configs.get("FILENAME_OUTPUT").data, mode='w',
+              encoding='utf-8') as message:
         message.write(content)
 
 
