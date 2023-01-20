75e0ee8a1225ae30b315f14f889ce7a6fa5567a1
Barbara Fauth <bg.fauth@gmail.com>
2023-01-20 22:26:12 +0100

    consider multiple block files

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+import pyperclip
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 import composer_utils as cu
 import input_utils
 import jinja_utils
-
-import pyperclip
 
 SERIES = "series"
 BLOCKS = "blocks"
@@ -22,14 +21,18 @@ def main():
 
     # select relevant series
     series_id = input_utils.get_relevant_series_id(series_json)
-    series_vars = cu.get_attribute_of_single_object(series_json, SERIES, series_id, "variables")
 
     # generate template from blocks
-    blocks_path = cu.get_attribute_of_single_object(series_json, SERIES, series_id, "source_file")
+    blocks_paths = cu.get_attribute_of_single_object(series_json, SERIES, series_id, "source_files")
+    blocks_json = cu.load_json_from_file(blocks_paths[str(0)])
+    for i in range(1, len(blocks_paths)):
+        blocks_i = cu.load_json_from_file(blocks_paths[str(i)])
+        for key, value in blocks_i.items():
+            if key in blocks_json:
+                blocks_json[key].extend(value)
+            else:
+                blocks_json[key] = value
     series_relevant_blocks = cu.get_attribute_of_single_object(series_json, SERIES, series_id, "blocks")
-
-    blocks_json = cu.load_json_from_file(blocks_path)
-
     cu.generate_template(series_relevant_blocks, blocks_json,
                          configs['Files']['TemplateFolder'] + configs['Files']['Template'])
     template = env.get_template(configs['Files']['Template'])
@@ -40,6 +43,7 @@ def main():
     time_vars = cu.flatten(cu.get_attribute_of_all_objects(blocks_json, BLOCKS, "time_vars"))
     enum_vars = cu.flatten(cu.get_attribute_of_all_objects(blocks_json, BLOCKS, "enum_vars"))
     optional_vars = cu.flatten(cu.get_attribute_of_all_objects(blocks_json, BLOCKS, "optional_vars"))
+    series_vars = cu.get_attribute_of_single_object(series_json, SERIES, series_id, "variables")
     text_variables_values = input_utils.get_template_vars(env, series_vars, int_vars, date_vars, time_vars,
                                                           enum_vars, optional_vars)
 
